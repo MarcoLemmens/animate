894078d10bc5ba844b7fd8aaec566211fd3ba809
Lemmens,Marco M.R.F <marco.lemmens@student.fontys.nl>
2016-06-19 10:28:46 +0200

    Added 1 optional parameter: - infinite (default: false)
    Added 1 animation:
    - elastic

--- animate.js
+++ animate.js
@@ -6,17 +6,24 @@
         var settings = $.extend({
             // These are the defaults.
             animation: "poster",
-            duration: 2
+            duration: 2,
+            infinite: false
         }, options );
+
+        var infinite;
+        if(settings.infinite == false){
+            infinite = "";
+        }
+        else{
+            infinite = "infinite ";
+        }
 
 
         var durationcss = settings.duration + "s";
 
-        $(this).addClass("animated " + settings.animation);
+        $(this).addClass("animated " + infinite + settings.animation);
         $(this).css("-webkit-animation-duration", durationcss);
         $(this).css("animation-duration", durationcss);
-
-
 
     };
 
